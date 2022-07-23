83824627d6db2d63648e3a51c241a73b3d743322
yanyuwangluo <70591645+yanyuwangluo@users.noreply.github.com>
2022-07-23 18:17:39 +0800

    更新视频解析插件（支持《我的框架》）

--- 视频聚合解析.js
+++ 视频聚合解析.js
@@ -1,5 +1,4 @@
 // [rule:视频解析 ?]
-sendText()
 var kw = param(1);
 kw = httpString(kw)[0];
 
@@ -10,15 +9,13 @@ var o = {
 }
 request(o, (err, resp, body)=>{
 	if(resp.statusCode == 200 && !err){
-		/* var text = ""
-		text += `标题：${body.title}\n视频连接：${body.url}` */
+		 var text = ""
+		text += `标题：${body.title}\n视频连接：${body.url}` 
 		sendText("正在查询, 请稍后..." + "\n支持列表：抖音/皮皮虾/火山/微视/微博/绿洲/最右/轻视频/instagram/哔哩哔哩/快手/全民小视频/皮皮搞笑全民k歌/巴塞电影/陌陌/Before避风/开眼/Vue Vlog/小咖秀/西瓜视频/逗拍")
-		let dwz = request({
-        url: "https://api.ooomn.com/api/dwz?type=aad&url=" + `${body.url}`,
-        dataType: "json",
-      }).short;
-      sendText(`标题：${body.title}\n视频连接：${dwz}`);
-		//sendText(text)
+		sendText(text)
+		//新增支持（《我的框架》）
+		//vlw请注释（sendVideo这一行）
+		sendVideo(`${body.url}`)
 	}else{
 		sendText("请求失败，请稍后再试。")
 	}
