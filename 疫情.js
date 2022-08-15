e26db00a988ea516a24796de3fa0db4010c11def
yanyuwangluo <70591645+yanyuwangluo@users.noreply.github.com>
2022-08-15 17:36:42 +0800

    疫情查询接口更新
    修复接口

--- 疫情.js
+++ 疫情.js
@@ -1,25 +1,19 @@
 /*
- * @Date: 2022-03-02 14:49:39
+ * @Date: 2022年8月15日17:36:01
  * @LastEditors: 烟雨
- * @LastEditTime: 2022-03-02 14:51:10
+ * @LastEditTime: 2022年8月15日17:36:01
  * @FilePath: \undefinedc:\Users\Administrator\Desktop\疫情.js
+ * 修复接口
  */
 // [rule: ?疫情 ] 武汉疫情
 // [rule: 疫情 ? ] 疫情 武汉
 function main() {
     var address = param(1) 
     var content = request({ 
-        "url": "https://api.iyk0.com/yq/?msg=" + address ,  //请求地址
+        "url": "https://xiaobai.klizi.cn/API/other/yiqing.php?msg=" + address ,  //请求地址
         "method": "get",   //请求方式
-        "dataType": "json"   //返回格式
+        //"dataType": "json"   //返回格式
     })
-        sendText("查询地区:" + content.查询地区+
-        "\n目前确诊：" + content.目前确诊 +
-        "\n死亡人数：" +  content.死亡人数 + 
-        "\n治愈人数："+ content.治愈人数 +
-        "\n新增确诊:"+ content.新增确诊 +
-        "\n现存确诊:"+ content.现存确诊 +
-        "\n现存无症状:"+ content.现存无症状 +
-        "\n更新时间:"+ content.time)
+        sendText(content)
 }
 main()
